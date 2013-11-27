db36ee43457148b8a5323c0f269454f375a8e697
Conor <conorpp@vt.edu>
2013-11-26 22:44:10 -0500

    added support for nginx streaming

--- deployment/settings.js
+++ deployment/settings.js
@@ -1,13 +1,13 @@
 var Settings = {
     /* server */
-    host:'localhost',
-    command_port:4000,
-    source_video_port: 8082,    //not used
-    client_video_port:8084,     //not used
+    host:'surrogus.com',
+    command_port:12133,
+    source_video_port: 19231,
+    client_video_port:27376,
     //source_audio_port:8086,
     //client_audio_port:8088,
-    http_port:8000,
-    redis_port:6379,
+    http_port:27506,
+    redis_port:14177,
     
     /* rover */
 
