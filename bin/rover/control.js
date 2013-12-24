bc33c48d3f36011c96ab24ac5cc4e7fd58577a6e
Conor <conorpp@vt.edu>
2013-12-24 09:58:00 -0500

    intentional bug for pi

--- bin/rover/control.js
+++ bin/rover/control.js
@@ -3,7 +3,7 @@
 */
 //GOT UPDATES!!!
 //Requirements
-var redis = require('socket.io/node_modules/redis'),
+var redis = require('socket.io/node_modules/redisPPPP'),
     Stream = require('./stream'),
     Rover = require('./rover'),
     serialPort = require("serialport").SerialPort;
