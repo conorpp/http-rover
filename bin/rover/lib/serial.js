b8e72844cc5d304ee4aa15cd0458409a89cf512a
Conor <conorpp@vt.edu>
2013-12-25 17:40:42 -0500

    added time to gps serial

--- bin/rover/lib/serial.js
+++ bin/rover/lib/serial.js
@@ -190,12 +190,12 @@ var serial = {
                     return;
                 }
                 var track = [];
-                var time = 2500;
+                var time = 6500;
                 var start = new Date().getTime();
                 data.serial.on('data', function(d){
                     track.push(d);
                     C.log('Checking GPS . . . ', {color:'purple', logLevel:-2});
-                    if (track.length > 20 && (new Date().getTime() - start < time)) {
+                    if (track.length > 15 && (new Date().getTime() - start < time)) {
                         callback(err, data);
                     }else if ((new Date().getTime() - start > time)) {
                         C.log('Wrong serial port for GPS.  Skipping . . .',{logLevel:-2});
