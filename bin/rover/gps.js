6e1e37f0ec6619eae62ef91cd1ae06f59b1b4532
Conor <conorpp@vt.edu>
2013-12-25 22:40:41 -0500

    pi testing

--- bin/rover/gps.js
+++ bin/rover/gps.js
@@ -125,13 +125,12 @@ var GPS = {
         this.gprmc+=h;
         console.log('ADDING ', h);
         var p = this.gprmc.length >= 6;
-        console.log('P is ', p);
+            console.log('progress ', this.gprmc);
+            console.log('length? ', this.gprmc.length);
         if (this.gprmc.length >= 6) {
             if (this.gprmc.substr(0,6) != '$GPRMC') {
                 this.redo();
             }
-            console.log('progress ', this.gprmc);
-            console.log('length? ', this.gprmc.length);
        }
         //if (i>79) {
         //    console.log('raw 2 GPS :', this.gprmc);
