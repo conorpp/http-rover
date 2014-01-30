26a4cbc7fc02a6f55a7d8945f5dc8987003057eb
Conor <conorpp@vt.edu>
2014-01-30 16:30:38 -0500

    tweaked the reset

--- bin/rover/stream.js
+++ bin/rover/stream.js
@@ -197,7 +197,7 @@ var Stream = {
         Terminal.exec('sudo pkill -SIGINT ffmpeg', function(){
             
             
-            Terminal.exec('lsusb',function(err, stdout, stderr){
+            /*Terminal.exec('lsusb',function(err, stdout, stderr){
                 var devices = stdout.match(/[^\n]+(?:\n|$)/g); //split by newlines
                 var device;
                 for (var i in devices) {
@@ -231,7 +231,8 @@ var Stream = {
                         callback(err, {});
                     }
                 });
-            });
+            });*/
+            callback(null, device);
         });
 
     },
